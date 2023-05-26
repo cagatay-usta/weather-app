0b947fe53ae6aa28b5ab114511770a4d6fbd23e7
cagatay-usta <cagatay_usta@windowslive.com>
2023-05-26 23:32:16 +0300

    complete all functionality

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import "./style.css";
 
-
 const name = document.querySelector(".location");
 const temp_c = document.querySelector(".temp");
 const wind_kph = document.querySelector(".wind");
@@ -11,26 +10,30 @@ const humidity = document.querySelector(".humidity");
 const condition = document.querySelector(".condition");
 const conditionSymbol = document.querySelector(".condition-symbol");
 
-
 async function getWeather(location) {
   try {
     const response = await fetch(
       `http://api.weatherapi.com/v1/current.json?key=db7a213f0b714cebb18125709231905&q=${location}&aqi=yes`
     );
     const weatherData = await response.json();
-    name.textContent = weatherData.location.name;
-    temp_c.textContent = weatherData.current.temp_c;
-    wind_kph.textContent = weatherData.current.wind_kph;
-    feelslike_c.textContent = weatherData.current.feelslike_c;
+    name.textContent = `${weatherData.location.name},`;
+    temp_c.textContent = `${weatherData.current.temp_c} C`;
+    wind_kph.textContent = `${weatherData.current.wind_kph} kph`;
+    feelslike_c.textContent = `${weatherData.current.feelslike_c} C`;
     country.textContent = weatherData.location.country;
     humidity.textContent = weatherData.current.humidity;
     condition.textContent = weatherData.current.condition.text;
     conditionSymbol.src = weatherData.current.condition.icon;
-
-    
   } catch (error) {
     console.log(error);
   }
 }
 let location = "london";
 getWeather(location);
+const searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", () => {
+  const locationInput = document.querySelector("#location-input");
+  if (locationInput.value) {
+    getWeather(locationInput.value);
+  }
+});
